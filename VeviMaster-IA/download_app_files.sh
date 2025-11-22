0ad57555530a13b298e07b0e11bef410a6afee44
Name <diego@gmail.com>
2025-11-22 23:23:11 +0100

    Update app_files.zip with Linux binary of phaselimiter

--- VeviMaster-IA/download_app_files.sh
+++ VeviMaster-IA/download_app_files.sh
@@ -15,7 +15,7 @@ else
     
     # Descargar el archivo comprimido desde Google Drive
     # REEMPLAZA 'YOUR_GOOGLE_DRIVE_FILE_ID' con el ID real de tu archivo
-    gdown "https://drive.google.com/uc?id=1SAm4l3MjK8Rg1PNSgKi_C0WLOEUIf3Or" -O app_files.zip
+    gdown "https://drive.google.com/uc?id=1CNe457Xc-m3DB4_W37Poba--IjXvnQ9k" -O app_files.zip
     
     # Descomprimir
     unzip -q app_files.zip -d vevi_mastering/
