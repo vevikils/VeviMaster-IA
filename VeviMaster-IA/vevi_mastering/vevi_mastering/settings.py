e38266db46a18c39b7e2c8b9cd53963cb693726d
Name <diego@gmail.com>
2025-11-27 14:38:41 +0100

    Fix: Usar sintaxis Path para TEMPLATES DIRS

--- VeviMaster-IA/vevi_mastering/vevi_mastering/settings.py
+++ VeviMaster-IA/vevi_mastering/vevi_mastering/settings.py
@@ -105,7 +105,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
