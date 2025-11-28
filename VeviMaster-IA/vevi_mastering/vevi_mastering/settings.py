eea28c704c178fd60e81cd0761068555b4273b71
Name <diego@gmail.com>
2025-11-28 14:07:37 +0100

    Fix: Cambiar LOGIN_REDIRECT_URL a mastering:upload

--- VeviMaster-IA/vevi_mastering/vevi_mastering/settings.py
+++ VeviMaster-IA/vevi_mastering/vevi_mastering/settings.py
@@ -90,7 +90,7 @@ AUTHENTICATION_BACKENDS = [
 
 # Configuración de Allauth
 LOGIN_URL = 'account_login'
-LOGIN_REDIRECT_URL = 'mastering:index'
+LOGIN_REDIRECT_URL = 'mastering:upload'
 LOGOUT_REDIRECT_URL = 'account_login'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
